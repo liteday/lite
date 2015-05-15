f0036b4e96e20b7b41c4a85417fdacae5e171e03
Peter Cautley <peter.cautley@ammeon.com>
2015-05-15 16:10:21 +0100

    update test

--- database/db_main.py
+++ database/db_main.py
@@ -12,11 +12,12 @@ if __name__ == '__main__':
 
     #test_database.load_systems({1:'val'})
     #test_database.load_systems(['sys1', 'sys2'])
-    test_database.load_systems({'sys1' : { 'serv1':'localhost101', 
-                                           'serv2':'localhost102' },
-                                'sys2' : { 'serv3':'localhost103', 
-                                           'serv4':'localhost104' } 
+    test_database.load_systems({'ts1' : { 'serv1':'localhost101', 
+                                          'serv2':'localhost102' },
+                                'ts2' : { 'serv3':'localhost101', 
+                                          'serv4':'localhost102' },
+                                'ts3' : { 'serv5':'localhost103', 
+                                          'serv6':'localhost104' } 
                                })
-    
     test_database.get_all_systems()
 
