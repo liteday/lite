cf2b3e1aa523ddcc9cf3f21a9c060c910d4d51d5
Dara McHugh <dara.mchugh@gmail.com>
2015-05-15 13:05:21 +0100

    controller class

--- controller/controller.py
+++ controller/controller.py
@@ -12,28 +12,24 @@ class Coordinator(object):
         
     def check_system(sys_id=0):
         # validate ID?
-        self.management_queue.put(task.CheckTask(sys_id))
-        return system_response
+        self.management_queue.put(task.CheckTask(sys_id))        
 
     def deploy_system(self,config):
-        if not True: #config.isValid():
-            raise Exception()        
         self.management_queue.put(task.DeployTask(config))
 
     def run_worker(self):
-        print "In run"
         self.worker_thread = threading.Thread(target=self.worker)
         self.worker_thread.daemon = True
         self.worker_thread.start()
 
     def process(self,item):
         print type(item)
-        pass
         
     def worker(self):
         while True:
+            print "In run"        
             try:
-                self.process(self.management_queue.get(block=False))            
+                self.process(self.management_queue.get(block=False))
             except Exception as e:
                 print "worker failed, ",str(e)
                 return
